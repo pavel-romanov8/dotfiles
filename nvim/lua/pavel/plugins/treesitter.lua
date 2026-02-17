b801b74f30018eadd4f5b1ad78ab8c8eb13227d7
Pavel Romanov <romanovp8913@gmail.com>
2026-02-17 10:53:32 +0200

    Adopted treesitter to match the latest branch changes

--- nvim/lua/pavel/plugins/treesitter.lua
+++ nvim/lua/pavel/plugins/treesitter.lua
@@ -1,59 +1,55 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	event = { "BufReadPre", "BufNewFile" },
+	branch = "main",
 	build = ":TSUpdate",
+	lazy = false,
 	config = function()
-		-- import nvim-treesitter plugin
-		local treesitter = require("nvim-treesitter.configs")
+		require("nvim-treesitter").setup({})
 
-		-- configure treesitter
-		treesitter.setup({ -- enable syntax highlighting
-			sync_install = true,
-			auto_install = false,
-			ignore_install = {},
-			-- ensure these language parsers are installed
-			ensure_installed = {
-				"json",
-				"javascript",
-				"typescript",
-				"tsx",
-				"yaml",
-				"html",
-				"css",
-				"prisma",
-				"markdown",
-				"markdown_inline",
-				"svelte",
-				"graphql",
-				"bash",
-				"lua",
-				"vim",
-				"dockerfile",
-					"query",
-				"vimdoc",
-				"c",
-				"angular",
-				"vue",
-				"go",
-				"terraform",
-				"toml",
-				"astro",
-			},
-			modules = {},
-			highlight = {
-				enable = true,
-			},
-			-- enable indentation
-			indent = { enable = true },
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "<C-space>",
-					node_incremental = "<C-space>",
-					scope_incremental = false,
-					node_decremental = "<bs>",
-				},
-			},
+		-- Install parsers (replaces ensure_installed)
+		local parsers = {
+			"json",
+			"javascript",
+			"typescript",
+			"tsx",
+			"yaml",
+			"html",
+			"css",
+			"prisma",
+			"markdown",
+			"markdown_inline",
+			"svelte",
+			"graphql",
+			"bash",
+			"lua",
+			"vim",
+			"dockerfile",
+			"query",
+			"vimdoc",
+			"c",
+			"angular",
+			"vue",
+			"go",
+			"terraform",
+			"toml",
+			"astro",
+		}
+		vim.schedule(function()
+			require("nvim-treesitter").install(parsers)
+		end)
+
+		-- Enable treesitter highlighting (replaces highlight.enable)
+		vim.api.nvim_create_autocmd("FileType", {
+			callback = function(args)
+				pcall(vim.treesitter.start, args.buf)
+			end,
+		})
+
+		-- Enable treesitter indentation (replaces indent.enable)
+		vim.api.nvim_create_autocmd("FileType", {
+			callback = function()
+				vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+			end,
 		})
 	end,
 }
