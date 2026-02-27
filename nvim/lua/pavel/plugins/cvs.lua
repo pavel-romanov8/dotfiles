4b7a83d2395353956c0d0b0ceb0d9d3f84557a3e
Pavel Romanov <romanovp8913@gmail.com>
2026-02-27 22:23:47 +0700

    Add CVS plugin

---
+++ nvim/lua/pavel/plugins/cvs.lua
@@ -0,0 +1,16 @@
+return {
+  "mrchnk/cvs.nvim",
+  commit = "8f5c68a6dbf1084c183e2335e9e1915e41e89337",
+  cmd = {
+    "CVSLog",
+    "CVSDiff",
+    "CVSCommit",
+    "CVSAnnotate",
+    "CVSAnnotateNextHunk",
+    "CVSAnnotatePrevHunk",
+  },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+  },
+}
